ceb0ed6fe02304298e567b0da87dc031da1993de
1emank <dev.1emank@gmail.com>
2025-08-30 09:25:16 +0200

    (refactor) tidying up some mistakes related to rsync (no longer in use)

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,28 +1,28 @@
-local function message(msg)
-    local buf = vim.api.nvim_create_buf(false, true)
-    vim.api.nvim_buf_set_lines(buf, 0, -1, false, { msg })
-
-    local width = vim.fn.strdisplaywidth(msg)
-    local height = 1
-
-    local opts = {
-        relative = 'editor',
-        width = width,
-        height = height,
-        col = (vim.o.columns - width) * 0.5,
-        row = (vim.o.lines - height) * 0.9,
-        style = 'minimal',
-        -- border = "rounded"
-    }
-
-    local win = vim.api.nvim_open_win(buf, false, opts)
-
-    vim.defer_fn(function()
-        if vim.api.nvim_win_is_valid(win) then
-            vim.api.nvim_win_close(win, true)
-        end
-    end, 3000)
-end
+-- local function message(msg)
+--     local buf = vim.api.nvim_create_buf(false, true)
+--     vim.api.nvim_buf_set_lines(buf, 0, -1, false, { msg })
+--
+--     local width = vim.fn.strdisplaywidth(msg)
+--     local height = 1
+--
+--     local opts = {
+--         relative = 'editor',
+--         width = width,
+--         height = height,
+--         col = (vim.o.columns - width) * 0.5,
+--         row = (vim.o.lines - height) * 0.9,
+--         style = 'minimal',
+--         -- border = "rounded"
+--     }
+--
+--     local win = vim.api.nvim_open_win(buf, false, opts)
+--
+--     vim.defer_fn(function()
+--         if vim.api.nvim_win_is_valid(win) then
+--             vim.api.nvim_win_close(win, true)
+--         end
+--     end, 3000)
+-- end
 
 ---@module "telescope.builtin"
 local t = require('telescope')
--- .config/nvim/lua/plugins/tog.lua
+++ .config/nvim/lua/plugins/tog.lua
@@ -1,4 +1,8 @@
-return {
-    dir = '~/repos/nvim/tog',
-    opts = {}
-}
+if os.execute('[ -d ~/repos/nvim/tog ]') == 0 then
+    return {
+        dir = '~/repos/nvim/tog',
+        opts = {}
+    }
+else
+    return {}
+end
